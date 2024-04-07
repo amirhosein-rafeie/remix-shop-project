3eb3bee91b0b1f918c73cbd8648d6b1aa9814cfd
Amirhosein Rafeie <rafeieamirhosein@gmail.com>
2024-04-07 16:21:26 +0330

    fix: config prisma

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, type LoaderFunction, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import prisma from "prisma/script";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,7 +9,16 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader: LoaderFunction = async () => {
+  const users = await prisma.user.findMany();
+  return json({ users });
+};
+
 export default function Index() {
+  const data = useLoaderData();
+
+  console.log(data);
+
   return (
     <main>
       <h1>Welcome to Remix Shop!</h1>
